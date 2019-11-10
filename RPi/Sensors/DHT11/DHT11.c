8c6dc7edd782d769463353e5afa7a2bc2275b605
isokolovic <isokolovic1@gmail.com>
2019-11-10 16:37:52 +0100

    Temporary DHT11 implementation

---
+++ RPi/Sensors/DHT11/DHT11.c
@@ -0,0 +1,103 @@
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAXTIMINGS 85
+#define DHTPIN 7
+
+int dht11_dat[5] = {0, 0, 0, 0, 0};
+
+void read_dht11_dat()
+{
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    float f; //fahrenheit
+
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0; //redundant?
+
+    // pull pin down for 18 ms
+    pinMode(DHTPIN, OUTPUT);
+    digitalWrite(DHTPIN, LOW);
+    delay(18);
+
+    // pull pin up for 40 ms
+    digitalWrite(DHTPIN, HIGH);
+    delay(40);
+
+    // prepare to read the pin
+    pinMode(DHTPIN, INPUT);
+
+    // detect changes for read data
+    for(i = 0; i < MAXTIMINGS; i++)
+    {
+        counter = 0;
+
+        while (digitalRead(DHTPIN) == laststate)
+        {
+            counter++;
+            delayMicroseconds(1);
+
+            if(counter == 255)
+            {
+                break;
+            }
+        }
+
+        laststate = digitalRead(DHTPIN);
+
+        if(counter == 255)
+        {
+            break;
+        }
+
+        // ignore first 3 transitions
+        if((i >= 4) && (i % 2 == 0))
+        {
+            // put each bit into the storage bytes
+            dht11_dat[j / 8] <<= 1;
+
+            if(counter > 16)
+            {
+                dht11_dat[j / 8] |= 1;
+                j++;
+            }
+        }
+
+        // check we read 40 bits (8 bit x 5) + verify checksum in last byte. Print it out if data is good
+        if( (j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
+        {
+            break;
+        }
+        else
+        {
+            printf("Data is currupted => skip \n");
+        }
+
+        int main(void)
+        {
+            if(wiringPiSetup() == -1)
+            {
+                exit(1);
+            }
+
+            while (1)
+            {
+                read_dht11_dat();
+                delay(1000); //refresh after 1 s
+            }
+            
+            return(0);
+
+        }
+        
+
+
+
+
+        
+
+    }
+
+}
---
+++ RPi/Sensors/DHT11/DHT11.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+import time
+
+def bin2dec(string_num):
+    return str(int(string_num, 2))
+
+data = []
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, GPIO.HIGHT)
+time.sleep(0.025)
+GPIO.output(4, GPIO.LOW)
+time.sleep(0.02)
+
+GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+for i in range(0, 500):
+    data.append(GPIO.input(4))
+
+bit_count = 0
+tmp = 0
+count = 0
+HumidityBit = ""
+TemperatureBit = ""
+crc = ""
+
+try:
+    while data[count] == 1:
+        tmp = 1
+        count += 1
+
+    for i in range(0, 32):
+        bit_count = 0
+
+        while data[count] == 0:
+            tmp = 1
+            count += 1
+
+        while data[count] == 1:
+            bit_count += 1
+            count += 1
+
+        if(bit_count > 3):
+            if(i >= 0 and i < 8):
+                HumidityBit += "1"
+            
+            if(i >= 16 and i < 24):
+                TemperatureBit += "1"
+
+        else:
+            if(i >= 0 and i < 8):
+                HumidityBit += "0"
+            
+            if(i >= 16 and i < 24):
+                TemperatureBit += "0"
+
+except:
+    print("ERR_RANGE")
+    exit(0)
+
+Humidity = bin2dec(HumidityBit)
+Temperature = bit2dec(TemperatureBit)
+
+if ((int(Humidity) + int(Temperature) - int(bin2dec(crc))) == 0):
+    print("Humidity: " + Humidity + "%")
+    print("Temperature: " + Temperature + "C")
+else:
+    print("ERR_CRC")
